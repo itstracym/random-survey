0c6b820ff10b0b8dc68a5fef7a903b2e9e8b9dc4
itstracym <111981283+itstracym@users.noreply.github.com>
2022-08-24 20:02:23 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/jZXMzbzpms8rQPh1A',
+	'https://forms.gle/4nRzCuBc9sQVrkqZ6'
 ];
